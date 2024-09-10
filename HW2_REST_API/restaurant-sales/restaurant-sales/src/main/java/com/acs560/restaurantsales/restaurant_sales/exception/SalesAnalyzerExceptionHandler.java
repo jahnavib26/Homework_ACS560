7b8ae7c6b208fbf590fb2e670f2b73e96c7ac35d
jahnavi <jahnavibollineni26@gmail.com>
2024-09-10 17:43:02 -0400

    HW2 REST API

---
+++ HW2_REST_API/restaurant-sales/restaurant-sales/src/main/java/com/acs560/restaurantsales/restaurant_sales/exception/SalesAnalyzerExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.acs560.restaurantsales.restaurant_sales.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class SalesAnalyzerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
